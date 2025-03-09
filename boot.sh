eb92c570ec60b32b90e3ef97a0c9678dd4648bb0
Player_X_YT <dont@contact.me>
2025-03-09 16:35:05 -0400

    Minimize requests on boot

--- boot.sh
+++ boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ $(cat last_ip) != $(curl ident.me) ]]; then
-	curl ident.me > last_ip
+current_ip=$(curl ident.me)
+
+if [[ $(cat last_ip) != $current_ip ]]; then
+	echo -n $current_ip > last_ip
 	curl https://www.duckdns.org/update/$(cat url)/$(cat key)
 fi
